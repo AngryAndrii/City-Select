c59d4b271f6592dabc3131eb98e57d1814695e5b
Andrii Chychkan <mywork1728@gmail>
2023-05-01 00:30:09 +0300

    added filter

--- script.js
+++ script.js
@@ -3,4 +3,37 @@
 //     .then((data) => console.log(data));
 
 import data from './city.list.json' assert { type: 'json' };
-console.log(data);
+
+// for (let i = 0; i <= data.length; i++) {
+//     let cityes =
+// }
+
+const cityListElement = document.querySelector("#city-list");
+let cityInputElement = document.querySelector("#city-input");
+
+let cityes = data.map(x => x.name);
+cityes.sort();
+console.log(cityes);
+loadData(cityes, cityListElement);
+
+function loadData(name, element) {
+    if (name) {
+        element.innerHTML = '';
+        let innerElement = '';
+        name.forEach((item) => {
+            innerElement += `
+            <li>${item}</li>`;
+        });
+
+        element.innerHTML = innerElement;
+    }
+}
+
+function filterName(name, searchText) {
+    return name.filter((x) => x.toLowerCase().includes(searchText.toLowerCase()));
+}
+
+cityInputElement.addEventListener('input', function() {
+    const filteredName = filterName(cityes, cityInputElement.value);
+    loadData(filteredName, cityListElement);
+})
