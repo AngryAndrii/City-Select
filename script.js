9bce91ecac9df2907651a7d7159f22baf0e5cf9e
Andrii Chychkan <mywork1728@gmail>
2023-04-30 18:51:22 +0300

    try to do request

---
+++ script.js
@@ -0,0 +1,6 @@
+// fetch('./city.list.json')
+//     .then((response) => response.json())
+//     .then((data) => console.log(data));
+
+import data from './city.list.json' assert { type: 'JSON' };
+console.log(data);
